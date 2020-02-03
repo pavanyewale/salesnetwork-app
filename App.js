302d17d6236276792c66127591a161adbe4c168c
Pavan Yewale <pavanyewale1996@gmail.com>
2020-02-04 02:47:41 +0530

    simple app created

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Welcome to Sales Network, India's Biggest Sales Network.</Text>
+    </>
+  );
+};
+
+export default App;
